9c1721ed78adfc2417e18d569d53902565883433
anonhyme <antoine.bouchard2@usherbrooke.ca>
2015-02-17 22:01:30 -0500

    a few bits tried to escape, but we caught them

---
+++ webapp/src/main/java/org/anonyme/helloWorld/FormBean.java
@@ -0,0 +1,19 @@
+package org.anonyme.helloWorld;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ * Created by antoine on 11/28/2014.
+ */
+public class FormBean {
+    @NotEmpty
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
